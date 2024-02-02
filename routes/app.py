c81c76a3c340fff45907f27b6af105aafb6f7b94
mamunozb <ma.munozb1@uniandes.edu.co>
2024-02-02 17:30:45 -0500

    actualización routes

--- routes/app.py
+++ routes/app.py
@@ -7,9 +7,9 @@ from modelos import db, Route
 from vistas import vistaRoutes
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rout.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/routes_db"
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = 'frase-secreta'
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
 app_context = app.app_context()
