d66352072e8f62bd8ea3f037adf06912f1a432cb
Jhon Sebastian Bohorquez Guerrero <js.bohorquezg1@uniandes.edu.co>
2024-02-06 18:56:49 -0500

    API logic posts

---
+++ posts/src/errors/errors.py
@@ -0,0 +1,35 @@
+class ApiError(Exception):
+    code = 422
+    description = "Default message"
+
+class InvalidDates(ApiError):
+    code = 412
+    description = "Invalid dates"
+
+class IncompleteParams(ApiError):
+    code = 400
+    description = "Bad request"
+
+class InvalidParams(ApiError):
+    code = 400
+    description = "Bad request"
+
+class Unauthorized(ApiError):
+    code = 401
+    description = "Unauthorized"
+
+class PostNotFoundError(ApiError):
+    code = 404
+    description = "Route does not exist"
+
+class ExternalError(ApiError):
+    code = 422 # Default
+    description = "External error"
+    
+class PostCreateError(ApiError):
+    code = 422 # Default
+    description = "La publicación ya esta creada"
+
+    def __init__(self, code):
+        self.code = code
+
